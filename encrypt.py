d289677ac292ec77f05d99733dbe67e7076ab21a
Olav Schettler <olav.schettler@dw.com>
2021-01-19 08:45:17 +0100

    #1. Added example

--- encrypt.py
+++ encrypt.py
@@ -1,18 +1,25 @@
 import os
+import sys
 from dotenv import load_dotenv
 load_dotenv()
 
 import xxtea
 
-with open('passwd') as f:
-    lines = f.readlines()
+def encrypt(fname):
+    with open(fname) as f:
+        lines = f.readlines()
 
-key = os.getenv("PASSWD").encode().ljust(16, b'\0')
+    key = os.getenv("PASSWD").encode().ljust(16, b'\0')
 
-for l in lines:
-    name, passwd = l.split('\t', 1)
-    passwd = passwd.rstrip()
-    enc = xxtea.encrypt_hex(passwd, key)
+    for l in lines:
+        name, passwd = l.split('\t', 1)
+        passwd = passwd.rstrip()
+        enc = xxtea.encrypt_hex(passwd, key)
 
-    print(name + '\t' + enc.decode("ascii"))
+        print(name + '\t' + enc.decode("ascii"))
 
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        encrypt(sys.argv[1])
+    else:
+        encrypt('passwd')
